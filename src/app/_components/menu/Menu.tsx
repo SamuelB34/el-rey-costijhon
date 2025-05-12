c308ccfb0b9e287125ecdc7808713e8810702be9
SamuelB34 <samuel.barragan34@hotmail.com>
2025-05-11 20:16:57 -0700

    Adds burger menu icon as SVG
    Adds a new SVG file for a burger menu icon.
    
    This commit introduces the burger.svg file to the public/images directory. The icon is intended for use in navigation menus, likely on smaller screens, to represent a collapsible menu.

--- src/app/_components/menu/Menu.tsx
+++ src/app/_components/menu/Menu.tsx
@@ -1,7 +1,13 @@
 import styles from "./Menu.module.scss"
 import { Destacado } from "@/app/_components/menu/components/destacado/Destacado"
 import { Articulo } from "@/app/_components/menu/components/articulo/Articulo"
-import { costillas, destacados } from "@/app/_components/menu/mock"
+import {
+	chicharrones,
+	costillas,
+	destacados,
+	hamburguesas,
+	papas,
+} from "@/app/_components/menu/mock"
 import { Fragment } from "react"
 
 export const Menu = () => {
@@ -23,6 +29,7 @@ export const Menu = () => {
 				</div>
 			</div>
 			<div className={styles.separator}></div>
+			{/*Costillas*/}
 			<div className={styles.menu__destacados}>
 				<span className={styles.subtitle}>Costillas</span>
 				{costillas.map((costilla, index) => (
@@ -32,6 +39,51 @@ export const Menu = () => {
 							name={costilla.name}
 							price={costilla.price}
 							description={costilla.description}
+						/>
+					</Fragment>
+				))}
+			</div>
+			<div className={styles.separator}></div>
+			{/*Hamburguesas*/}
+			<div className={styles.menu__destacados}>
+				<span className={styles.subtitle}>Hamburguesas</span>
+				{hamburguesas.map((hamburguesa, index) => (
+					<Fragment key={index}>
+						<Articulo
+							imageUrl={hamburguesa.imageUrl}
+							name={hamburguesa.name}
+							price={hamburguesa.price}
+							description={hamburguesa.description}
+						/>
+					</Fragment>
+				))}
+			</div>
+			<div className={styles.separator}></div>
+			{/*Papas*/}
+			<div className={styles.menu__destacados}>
+				<span className={styles.subtitle}>Papas</span>
+				{papas.map((papa, index) => (
+					<Fragment key={index}>
+						<Articulo
+							imageUrl={papa.imageUrl}
+							name={papa.name}
+							price={papa.price}
+							description={papa.description}
+						/>
+					</Fragment>
+				))}
+			</div>
+			<div className={styles.separator}></div>
+			{/*Chicharrones*/}
+			<div className={styles.menu__destacados}>
+				<span className={styles.subtitle}>Chicharrones</span>
+				{chicharrones.map((chicharron, index) => (
+					<Fragment key={index}>
+						<Articulo
+							imageUrl={chicharron.imageUrl}
+							name={chicharron.name}
+							price={chicharron.price}
+							description={chicharron.description}
 						/>
 					</Fragment>
 				))}
--- src/app/_components/menu/mock.ts
+++ src/app/_components/menu/mock.ts
@@ -29,20 +29,80 @@ export const destacados = [
 export const costillas = [
 	{
 		imageUrl: "/images/costillas.svg",
-		name: "Charola - 1.2 kg",
+		name: "Charola",
 		price: 475,
 		description: "1.2 kg de costillas, 5 elotes y 5 papas",
 	},
 	{
 		imageUrl: "/images/costillas.svg",
-		name: "Charola - 1.2 kg",
-		price: 475,
-		description: "1.2 kg de costillas, 5 elotes y 5 papas",
+		name: "Charola Gratinada",
+		price: 525,
+		description:
+			"1.2 kg de costillas, 5 elotes y 5 papas gratinadas con tocino",
 	},
 	{
 		imageUrl: "/images/costillas.svg",
-		name: "Charola - 1.2 kg",
-		price: 475,
-		description: "1.2 kg de costillas, 5 elotes y 5 papas",
+		name: "Costijhon",
+		price: 370,
+		description: "850 gr de costilla, 3 elotes y 2 papas",
+	},
+	{
+		imageUrl: "/images/costillas.svg",
+		name: "Duo",
+		price: 270,
+		description: "500 gr de costilla, 2 elotes y 1 papa",
+	},
+	{
+		imageUrl: "/images/costillas.svg",
+		name: "Individual",
+		price: 170,
+		description: "250 gr de costilla, 1 elote y 1 papa",
+	},
+]
+
+export const hamburguesas = [
+	{
+		imageUrl: "/images/burger.svg",
+		name: "Burger Calijhona",
+		price: 120,
+		description:
+			"Carne de res, tocino, cebolla caramelizada, verduras y papas fritas",
+	},
+]
+
+export const papas = [
+	{
+		imageUrl: "/images/papa.svg",
+		name: "Papa gratinada",
+		price: 120,
+		description:
+			"Papa al horno rellena de carne asada, queso gratinado, tocino y aderezos",
+	},
+]
+
+export const chicharrones = [
+	{
+		imageUrl: "/images/porkbelly.svg",
+		name: "Chicharron porkbelly - 1 kg",
+		price: 450,
+		description: "1 kg de chicharron porkbelly con guacamole, salsa y limon",
+	},
+	{
+		imageUrl: "/images/porkbelly.svg",
+		name: "Chicharron porkbelly - 750 gr",
+		price: 350,
+		description: "750 gr de chicharron porkbelly con guacamole, salsa y limon",
+	},
+	{
+		imageUrl: "/images/porkbelly.svg",
+		name: "Chicharron porkbelly - 500 gr",
+		price: 250,
+		description: "500 gr de chicharron porkbelly con guacamole, salsa y limon",
+	},
+	{
+		imageUrl: "/images/porkbelly.svg",
+		name: "Chicharron porkbelly - 250 gr",
+		price: 150,
+		description: "250 gr de chicharron porkbelly con guacamole, salsa y limon",
 	},
 ]
